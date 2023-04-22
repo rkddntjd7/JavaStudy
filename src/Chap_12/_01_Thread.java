ee6b9d54f6f7613b41fafaf6a60ac78c4d956a21
rkddntjd7 <125332196+rkddntjd7@users.noreply.github.com>
2023-04-23 00:21:16 +0900

    0423 0020

---
+++ src/Chap_12/_01_Thread.java
@@ -0,0 +1,36 @@
+package Chap_12;
+
+import Chap_12.clean.CleanThread;
+
+public class _01_Thread {
+    public static void main(String[] args) {
+        // 하나의 프로세스 (Process)
+        // 쓰레드 (Thread)
+
+        // 1 3 5 7 9
+        // 2 4 6 8 10
+
+        // cleanBySelf();
+        CleanThread cleanThread = new CleanThread();
+        // cleanThread.run(); // 직원 청소하는거
+        cleanThread.start();
+
+        cleanByBoss(); // 사장님이 청소하는거
+    }
+
+    public static void cleanBySelf() {
+        System.out.println("-- 혼자서 청소 시작 --");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("(혼자) " + i + "번방 청소 중");
+        }
+        System.out.println("혼자 청소 끝.");
+    }
+
+    public static void cleanByBoss() {
+        System.out.println("-- 사장 청소 시작 --");
+        for (int i = 1; i <= 10; i+= 2) {
+            System.out.println("(사장) " + i + "번방 청소 중");
+        }
+        System.out.println("사장 청소 끝.");
+    }
+}
